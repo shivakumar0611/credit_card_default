09e48cc82f8817462196fcbcf48ab32b133f2d7f
shivakumar0611 <ayaank313@gmail.com>
2024-01-17 17:49:29 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="shiva",
+    author_email="shivak500@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
